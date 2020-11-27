5a3420e161357578a01fbc88c6bcb41b510ae9b6
unknown <#archgowda124@gmail.com>
2020-11-27 14:59:38 +0530

    Refactored:checks valid first name

--- UserRegistrationProblem.sh
+++ UserRegistrationProblem.sh
@@ -1,2 +1,15 @@
 #Welcome to User Registration Module
+#!/bin/bash
+shopt -s extglob
+read -p "Enter First Name: " First
+echo $First
+
+patt='^[A-Z]{1}[a-z]{2,}$'
+if [[ $First =~ $patt ]]
+then
+     echo " Name is valid "
+else
+     echo " Name is invalid "
+fi
+
 
